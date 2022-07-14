da5081f1deb4470e8c0cb303161eaeab48c98ef7
Galuh Idam <galuh.danutirto@opencloud.id>
2022-07-14 12:21:15 +0700

    Membuat Create, Read, Update, Delete

---
+++ app/Http/Controllers/StrategicInitiativeController.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\StrategicInitiatives;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class StrategicInitiativeController extends Controller
+{
+    public function index(Request $request)
+    {
+        $search             = $request->get('search');
+        $search_name        = $request->get('name');
+        $search_description = $request->get('description');
+
+        if ($request->get('order') && $request->get('by')) {
+            $order = $request->get('order');
+            $by = $request->get('by');
+        } else {
+            $order = 'id';
+            $by = 'desc';
+        }
+
+        if ($request->get('paginate')) {
+            $paginate = $request->get('paginate');
+        } else {
+            $paginate = 10;
+        }
+
+        $strategic_initiative = StrategicInitiatives::when($search, function ($query) use ($search) {
+            $query->where(function ($sub_query) use ($search) {
+                $sub_query->where('name', 'LIKE', "%{$search}%")
+                    ->orWhere('description', 'LIKE', "%{$search}%");
+            });
+        })->when($search_name, function ($query) use ($search_name) {
+            $query->where('name', 'LIKE', "%{$search_name}%");
+        })->when($search_description, function ($query) use ($search_description) {
+            $query->where('description', 'LIKE', "%{$search_description}%");
+        })->when(($order && $by), function ($query) use ($order, $by) {
+            $query->orderBy($order, $by);
+        })->paginate($paginate);
+
+        $query_string = [
+            'search' => $search,
+            'order' => $order,
+            'by' => $by,
+        ];
+
+        $strategic_initiative->appends($query_string);
+
+        return $strategic_initiative;
+    }
+
+    public function create(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|unique:strategic_initiatives|max:100',
+                'description' => 'required|max:100',
+            ]
+        );
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $strategic_initiative = StrategicInitiatives::create([
+            'name'        => $request->get('name'),
+            'description' => $request->get('description'),
+        ]);
+
+        return response()->json([
+            'message' => 'Strategic Initiative has created!',
+            'data' => $strategic_initiative,
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        $strategic_initiative = StrategicInitiatives::find($id);
+        if ($strategic_initiative) {
+            return response()->json($strategic_initiative, 302);
+        } else {
+            return response()->json([
+                'message' => 'Data not found!',
+            ], 404);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        $strategic_initiative = StrategicInitiatives::find($id);
+
+        if ($strategic_initiative) {
+            $validator = Validator::make(
+                $request->all(),
+                [
+                    'name'        => 'required|unique:strategic_initiatives,name,' . $id . '|max:100',
+                    'description' => 'required|max:100',
+                ]
+            );
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+
+            $strategic_initiative = StrategicInitiatives::where('id', $id)->update($request->all());
+            $data = StrategicInitiatives::where('id', $id)->first();
+
+            return response()->json([
+                'message' => 'Strategic Initiative has updated!',
+                'data' => $data,
+            ], 202);
+        } else {
+            return response()->json([
+                'message' => 'Data not found!',
+            ], 404);
+        }
+    }
+    public function destroy($id)
+    {
+        if ($id) {
+            $strategic_initiative = StrategicInitiatives::where('id', $id)->first();
+            if ($strategic_initiative) {
+                $strategic_initiative->delete();
+                return response()->json([
+                    'message' => 'Strategic Initiative has deleted!',
+                    'data'    => $strategic_initiative
+                ], 202);
+            } else {
+                return response()->json([
+                    'message' => 'Data not found !',
+                ], 404);
+            }
+        } else {
+            return response()->json([
+                'message' => 'Data not found !',
+            ], 404);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProspectController;
+use App\Http\Controllers\StrategicInitiativeController;
 use App\Http\Controllers\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
@@ -15,6 +16,13 @@ Route::group(['middleware' => ['role:super-admin']], function () {
         Route::get('prospect', [ProspectController::class, 'index']);
         Route::post('prospect-create', [ProspectController::class, 'create']);
         Route::get('users', [UserController::class, 'index']);
+
+        //Strategic Initiative Routes
+        Route::get('strategic-initiative', [StrategicInitiativeController::class, 'index']);
+        Route::post('strategic-initiative-create', [StrategicInitiativeController::class, 'create']);
+        Route::get('strategic-initiative-show', [StrategicInitiativeController::class, 'show']);
+        Route::post('strategic-initiative-update', [StrategicInitiativeController::class, 'update']);
+        Route::delete('strategic-initiative-delete', [StrategicInitiativeController::class, 'delete']);
     });
 });
 
