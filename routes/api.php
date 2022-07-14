7144fdfd65ef6768d51ef578a0549f0dadff1941
Galuh Idam <galuh.danutirto@opencloud.id>
2022-07-14 13:32:22 +0700

    Membuat Create, Read, Update, Delete

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProspectController;
 use App\Http\Controllers\UserController;
+use App\Models\Maintenance;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
@@ -12,9 +13,18 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['role:super-admin']], function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('users', [UserController::class, 'index']);
+
+        //Prospect Routes
         Route::get('prospect', [ProspectController::class, 'index']);
         Route::post('prospect-create', [ProspectController::class, 'create']);
-        Route::get('users', [UserController::class, 'index']);
+
+        //Maintenance Routes
+        Route::get('maintenance', [Maintenance::class, 'index']);
+        Route::post('maintenance-create', [Maintenance::class, 'create']);
+        Route::get('maintenance-show/{id}', [Maintenance::class, 'show']);
+        Route::post('maintenance-update/{id}', [Maintenance::class, 'update']);
+        Route::delete('maintenance-delete/{id}', [Maintenance::class, 'destroy']);
     });
 });
 
