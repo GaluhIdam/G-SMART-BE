b2516480d815ffdceb0b1968d3354775b394bb16
Galuh Idam <galuh.danutirto@opencloud.id>
2022-07-14 12:01:17 +0700

    Update ERD -> Set NULL to Cascade

--- database/migrations/2022_07_11_051151_create_prospects_table.php
+++ database/migrations/2022_07_11_051151_create_prospects_table.php
@@ -19,23 +19,23 @@ return new class extends Migration
 
             $table->unsignedBigInteger('transaction_type_id')->nullable();
             $table->index('transaction_type_id');
-            $table->foreign('transaction_type_id')->references('id')->on('transaction_types')->onDelete('set null');
+            $table->foreign('transaction_type_id')->references('id')->on('transaction_types')->onDelete('cascade');
 
             $table->unsignedBigInteger('prospect_type_id')->nullable();
             $table->index('prospect_type_id');
-            $table->foreign('prospect_type_id')->references('id')->on('prospect_types');
+            $table->foreign('prospect_type_id')->references('id')->on('prospect_types')->onDelete('cascade');
 
             $table->unsignedBigInteger('strategic_initiative_id')->nullable();
             $table->index('strategic_initiative_id');
-            $table->foreign('strategic_initiative_id')->references('id')->on('strategic_initiatives');
+            $table->foreign('strategic_initiative_id')->references('id')->on('strategic_initiatives')->onDelete('cascade');
 
             $table->unsignedBigInteger('pm_id')->nullable();
             $table->index('pm_id');
-            $table->foreign('pm_id')->references('id')->on('users');
+            $table->foreign('pm_id')->references('id')->on('users')->onDelete('cascade');
 
             $table->unsignedBigInteger('customer_id')->nullable();
             $table->index('customer_id');
-            $table->foreign('customer_id')->references('id')->on('customers');
+            $table->foreign('customer_id')->references('id')->on('customers')->onDelete('cascade');
             $table->timestamps();
         });
     }
