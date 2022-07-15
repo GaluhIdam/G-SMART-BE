59805a371ff6fc2d8a749069dea16a3f8b780591
Galuh Idam <galuh.danutirto@opencloud.id>
2022-07-15 10:40:01 +0700

    Create, Read, Update, Delete

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProspectController;
+use App\Http\Controllers\RegionController;
 use App\Http\Controllers\UserController;
 
 Route::post('register', [AuthController::class, 'register']);
@@ -12,9 +13,18 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['role:super-admin']], function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('users', [UserController::class, 'index']);
+
+        //Prospect Routes
         Route::get('prospect', [ProspectController::class, 'index']);
         Route::post('prospect-create', [ProspectController::class, 'create']);
-        Route::get('users', [UserController::class, 'index']);
+
+        //Region Routes
+        Route::get('region', [RegionController::class, 'index']);
+        Route::post('region-create', [RegionController::class, 'create']);
+        Route::get('region-show/{id}', [RegionController::class, 'show']);
+        Route::post('region-update/{id}', [RegionController::class, 'update']);
+        Route::delete('region-delete/{id}', [RegionController::class, 'destroy']);
     });
 });
 
