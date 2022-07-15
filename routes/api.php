5ec3f694b8e6b013c13365e7a487a03849f0f183
Galuh Idam <galuh.danutirto@opencloud.id>
2022-07-15 10:28:05 +0700

    Create, Read, Update, Delete

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AreaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
@@ -12,9 +13,18 @@ Route::post('login', [AuthController::class, 'login']);
 Route::group(['middleware' => ['role:super-admin']], function () {
     Route::middleware('auth:sanctum')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('users', [UserController::class, 'index']);
+
+        //Prospect Routes
         Route::get('prospect', [ProspectController::class, 'index']);
         Route::post('prospect-create', [ProspectController::class, 'create']);
-        Route::get('users', [UserController::class, 'index']);
+
+        //Area Routes
+        Route::get('area', [AreaController::class, 'index']);
+        Route::post('area-create', [AreaController::class, 'create']);
+        Route::get('area-show/{id}', [AreaController::class, 'show']);
+        Route::post('area-update/{id}', [AreaController::class, 'update']);
+        Route::delete('area-delete/{id}', [AreaController::class, 'destroy']);
     });
 });
 
