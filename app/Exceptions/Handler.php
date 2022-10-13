20aced8d7658685a214c1418b5ddceb199dfe8c3
Okitora <wiki.tora99@gmail.com>
2022-10-13 18:24:14 +0700

    [update] menambahkan custom exception untuk handling user role & permission

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,4 +50,16 @@ class Handler extends ExceptionHandler
             ), 422);
         });
     }
+
+    // Custom exception for spatie role permission
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof \Spatie\Permission\Exceptions\UnauthorizedException) {
+            return response()->json([
+                'success' => false,
+                'message' => 'You are unauthorized',
+            ], 403);
+        }
+        return parent::render($request, $exception);
+    }
 }
