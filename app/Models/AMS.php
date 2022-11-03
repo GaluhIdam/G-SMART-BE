c6109f4cb359acc249ec3b6dda382fe454b9a3e1
Okitora <wiki.tora99@gmail.com>
2022-11-03 15:09:01 +0700

    [fix] perbaikan pada sorting & searching tabel master ams

--- app/Models/AMS.php
+++ app/Models/AMS.php
@@ -16,6 +16,26 @@ class ams extends Model
         'user_id',
     ];
 
+    public function scopeSearch($query, $search)
+    {
+        $query->when($search, function ($query) use ($search) {
+            $query->where('initial', 'LIKE', "%{$search}%")
+                ->orWhereRelation('user', 'name', 'LIKE', "%{$search}%");
+        });
+    }
+
+    public function scopeSort($query, $order, $by)
+    {
+        $query->when(($order && $by), function ($query) use ($order, $by) {
+            if ($order  == 'user') {
+                $query->withAggregate('user', 'name')
+                    ->orderBy('user_name', $by);
+            } else {
+                $query->orderBy('initial', $by);
+            }
+        }) ;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class, 'user_id');
