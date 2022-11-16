1627c49fd0093cf77603275ab3ac04cee212909d
Okitora <wiki.tora99@gmail.com>
2022-11-16 20:47:03 +0700

    [update] custom request validation untuk proses create prospect

--- app/Http/Requests/ProspectRequest.php
+++ app/Http/Requests/ProspectRequest.php
@@ -17,14 +17,6 @@ class ProspectRequest extends FormRequest
         return true;
     }
 
-    // public function attributes()
-    // {
-    //     return [
-    //         'prospect_type_id' => '',
-    //         'transaction_type_id' => '',
-    //     ];
-    // }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -32,66 +24,66 @@ class ProspectRequest extends FormRequest
      */
     public function rules()
     {
-        // if (!$this->attributes->has('prospect_type_id')) {
-        //     return false;
-        // }
-
-        // if (!$this->attributes->has('transaction_type_id')) {
-        //     return false;
-        // }
-
-        $rules = [];
-
         $prospect_type = Request::input('prospect_type_id');
         $transaction_type = Request::input('transaction_type_id');
 
-        if ($prospect_type == 1) {
-            $prospect_rules = [
-                'year' => 'required|date_format:Y',
-                'ams_customer_id' => 'required|integer|exists:ams_customers,id',
-            ];
-        } else if ($prospect_type == 2) {
-            $prospect_rules = [
-                'year' => 'required|date_format:Y',
-                'ams_customer_id' => 'required|integer|exists:ams_customers,id',
-                'strategic_initiative_id' => 'required|integer|exists:strategic_initiatives,id',
-                'pm_id' => 'required|integer|exists:users,id',
+        if (($prospect_type && in_array($prospect_type, [1,2])) && 
+            ($transaction_type && in_array($transaction_type, [1,2,3]))) {
+            if ($prospect_type == 1) {
+                $prospect_rules = [
+                    'year' => 'required|date_format:Y',
+                    'ams_customer_id' => 'required|integer|exists:ams_customers,id',
+                ];
+            } else if ($prospect_type == 2) {
+                $prospect_rules = [
+                    'year' => 'required|date_format:Y',
+                    'ams_customer_id' => 'required|integer|exists:ams_customers,id',
+                    'strategic_initiative_id' => 'required|integer|exists:strategic_initiatives,id',
+                    'pm_id' => 'required|integer|exists:users,id',
+                ];
+            }
+    
+            if (in_array($transaction_type, [1,2])) {
+                $transaction_rules = [
+                    'tmb' => 'required|array',
+                    'tmb.*.product' => 'required|array',
+                    'tmb.*.product.*.product_id' => 'required|integer|exists:products,id',
+                    'tmb.*.product.*.aircraft_type' => 'sometimes|required',
+                    'tmb.*.product.*.component' => 'sometimes|required',
+                    'tmb.*.product.*.apu' => 'sometimes|required',
+                    'tmb.*.product.*.engine' => 'sometimes|required',
+                    'tmb.*.product.*.market_share'  => 'required|numeric',
+                    'tmb.*.product.*.remark' => 'required|string',
+                    'tmb.*.product.*.maintenance_id' => 'required',
+                ];
+            } else if ($transaction_type == 3) {
+                $transaction_rules = [
+                    'pbth' => 'required|array',
+                    'pbth.*.product_id' => 'required|integer|exists:products,id',
+                    'pbth.*.aircraft_type_id' => 'required|integer|exists:ac_type_id,id',
+                    'pbth.*.target' => 'required|array',
+                    'pbth.*.target.*.month' => 'required|date_format:F',
+                    'pbth.*.target.*.rate' => 'required|numeric',
+                    'pbth.*.target.*.flight_hour' => 'required|numeric',
+                ];
+            }
+
+            return array_merge($prospect_rules, $transaction_rules);
+        } else {
+            return [
+                'prospect_type_id' => 'required|between:1,2',
+                'transaction_type_id' => 'required|between:1,3',
             ];
         }
-
-        if (in_array($transaction_type, [1,2])) {
-            $transaction_rules = [
-                'tmb' => 'required|array',
-                'tmb.*.product' => 'required|array',
-                'tmb.*.product.*.product_id' => 'required|integer|exists:products,id',
-                'tmb.*.product.*.aircraft_type.id' => 'required|integer|exists:ac_type_id,id',
-                'tmb.*.product.*.component.id' => 'required|integer|exists:component_id,id',
-                'tmb.*.product.*.apu.id' => 'required|integer|exists:apu_id,id',
-                'tmb.*.product.*.engine.id' => 'required|integer|exists:engine_id,id',
-                'tmb.*.product.*.market_share'  => 'required|numeric',
-                'tmb.*.product.*.remark' => 'required|string',
-                'tmb.*.product.*.maintenance_id.id' => 'required|integer|exists:maintenances,id',
-            ];
-        } else if ($transaction_type == 3) {
-            $transaction_rules = [
-                'pbth' => 'required|array',
-                'pbth.*.product_id' => 'required|integer|exists:products,id',
-                'pbth.*.aircraft_type_id' => 'required|integer|exists:ac_type_id,id',
-                'pbth.*.target' => 'required|array',
-                'pbth.*.target.*.month' => 'required|date_format:F',
-                'pbth.*.target.*.rate' => 'required|numeric',
-                'pbth.*.target.*.flight_hour' => 'required|numeric',
-            ];
-        }
-
-        $rules = array_merge($prospect_rules, $transaction_rules);
-
-        return $rules;
     }
 
     public function messages()
     {
         return [
+            'prospect_type_id.required' => 'The Prospect Type is required.',
+            'prospect_type_id.between' => 'The Prospect Type must be between Organic and In-organic.',
+            'transaction_type_id.required' => 'The Transaction Type is required.',
+            'transaction_type_id.between' => 'The Transaction must be between TMB Retail, TMB Project and PBTH.',
             'year.required' => 'The Year field is required.',
             'year.date_format' => 'The Year format is invalid.',
             'ams_cuseromer_id.required' => 'The AMS & Area field is required.',
@@ -106,16 +98,15 @@ class ProspectRequest extends FormRequest
             'tmb.*.product.array' => 'The Product data collection must be an array.',
             'tmb.*.product.*.product_id.required' => 'The Product field is required.',
             'tmb.*.product.*.product_id.exists' => 'The selected Product is invalid.',
-            'tmb.*.product.*.aircraft_type.id.required' => 'The Aircraft Type field is required.',
-            'tmb.*.product.*.aircraft_type.id.exists' => 'The selected Aircraft Type is invalid.',
-            'tmb.*.product.*.component.id.required' => 'The Component field is required.',
-            'tmb.*.product.*.component.id.exists' => 'The selected Component is invalid.',
-            'tmb.*.product.*.apu.id.required' => 'The APU field is required.',
-            'tmb.*.product.*.apu.id.exists' => 'The selected APU is invalid.',
-            'tmb.*.product.*.engine.id.required' => 'The Engine field is required.',
-            'tmb.*.product.*.engine.id.exists' => 'The selected Engine is invalid.',
+            'tmb.*.product.*.aircraft_type.required' => 'The Aircraft Type field is required.',
+            'tmb.*.product.*.component.required' => 'The Component field is required.',
+            'tmb.*.product.*.apu.required' => 'The APU field is required.',
+            'tmb.*.product.*.engine.required' => 'The Engine field is required.',
             'tmb.*.product.*.market_share.required' => 'The Market Share field is required.',
             'tmb.*.product.*.market_share.numeric' => 'The Market Share field must be a number.',
+            'tmb.*.product.*.remark.required' => 'The Remarks field is required.',
+            'tmb.*.product.*.remark.string' => 'The Remarks field must be a string.',
+            'tmb.*.product.*.maintenance_id.required' => 'The Maintenance field is required.',
             'pbth.required' => 'The PBTH data collection is required.',
             'pbth.array' => 'The PBTH data colection must be an array.',
             'pbth.*.product_id.required' => 'The Product field is required.',
