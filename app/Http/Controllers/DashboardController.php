d07910170bd7890e955bc67a4114743e63d21f97
Surya Intan Permana <surya.permana@opencloud.id>
2022-12-19 14:42:28 +0700

    [update] merubah urutan product chart

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -80,7 +80,18 @@ class DashboardController extends Controller
     {
         $user = auth()->user();
 
-        $products = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        // TODO urutannya diubah 
+        $products = [
+            9, 
+            8, 
+            7, 
+            6, 
+            5, 
+            4, 
+            2, 
+            1, 
+            3
+        ];
         $array_target = [];
         $array_progress = [];
         $array_percentage = [];
